9bf5c6a934e7019d65af54a3a047aad2440dff3b
!Human <itz.broken.xd@gmail.com>
2024-10-02 10:40:20 +0530

    Squashed commit of the following:
    commit f7baea83a9ac7a7494d7537f108e85cf88c27312
    Author: !Human <127822226+n0t-Human@users.noreply.github.com>
    Date:   Wed Oct 2 10:30:49 2024 +0530
    
        Update IRC.ino
    
        Disable debug by default and added few more debug statements
    
    commit 3b2b73ad34f43cf6191e82764a62a2290621120f
    Author: !Human <127822226+n0t-Human@users.noreply.github.com>
    Date:   Wed Oct 2 10:24:23 2024 +0530
    
        Update README.md
    
    commit 78f9a2d3da0e0fd384aa5bf68a339ea4a6e6ec9c
    Author: !Human <itz.broken.xd@gmail.com>
    Date:   Tue Oct 1 23:54:00 2024 +0530
    
        Update IRC.ino
    
    commit dc455cc7b09118d82181647cce7a07b6ecb87f2a
    Author: !Human <itz.broken.xd@gmail.com>
    Date:   Mon Sep 30 18:38:00 2024 +0530
    
        bug fix
    
    commit 264505e6d8aff6c97b89c278681a54d6a36a9370
    Author: !Human <itz.broken.xd@gmail.com>
    Date:   Mon Sep 30 00:55:39 2024 +0530
    
        Update IRC.ino

--- IRC.ino
+++ IRC.ino
@@ -1,123 +1,121 @@
-#include <DabbleESP32.h>
-#include <ESP32Servo.h>
+#define CUSTOM_SETTINGS
+#define INCLUDE_GAMEPAD_MODULE
 
-// Setting PWM properties
-const uint16_t freq[] = {30000 , 30000} ;
-const byte pwmChannel[] = {0 , 1};
-const byte resolution = 8;
-const byte dutyCycle = 200;
+#include <DabbleESP32.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
 
 // Movement
-const byte motorPin_fwd[] = {16 , 17};   // IN Pins
-const byte motorPin_bkwrd[] = { 25, 26}; // IN  pins
-const byte motorPin_speed[] = {14 , 12}; // ENA Pins for Speed
+const byte motorPin_fwd[] = {16 , 5};   // IN Pins
+const byte motorPin_bkwrd[] = { 17, 18}; // IN  pins
 const uint8_t movementDelay = 250; // Determines a minimum delay upto which robot will move for a single input
 
 
 // Arm
-const byte servoPin[] = { 18, 19, 21, 23 };
-Servo servo[4];
-byte angle[] = { 0, 0, 0, 0 };
-byte servoSelected = 0;
-const uint16_t servoDelay[] = { 50, 50, 50, 50 };
+
+Adafruit_PWMServoDriver servoDriver = Adafruit_PWMServoDriver(0x40);
+
+#define servoMIN  150 // This is the 'minimum' pulse length count (out of 4096)
+#define servoMAX  600 // This is the 'maximum' pulse length count (out of 4096)
+
+uint8_t min_val[] = { 90 , 0 , 0 , 0 , 0 , 0 };
+uint8_t max_val[] = { 180 , 180 , 180 , 180 , 180 , 180 };
+uint8_t servoDelay[] = { 50 , 50 , 50 , 50 , 50 , 50 }; // customize sensitivity for each servo
+uint8_t value[] = { 0 , 0 , 0 , 0 , 0 , 0 };
+byte servo = 0;
 
 // if true , in  movement mode
 bool mode = false;
+bool debug = false;
 
 void setup() {
   for (int i = 0; i < 4; i++) {
     servo[i].attach(servoPin[i]);
-    
-    if(i < 2) {
-      pinMode(motorPin_fwd[i], OUTPUT);
-      pinMode(motorPin_bkwrd[i], OUTPUT);
-      pinMode(motorPin_speed[i], OUTPUT);
-    }
+
+    pinMode(motorPin_fwd[i], OUTPUT);
+    pinMode(motorPin_bkwrd[i], OUTPUT);
+    pinMode(motorPin_speed[i], OUTPUT);
   }
   pinMode(LED_BUILTIN, OUTPUT);
 
+  servoDriver.begin();
+  servoDriver.setPWMFreq(60);
 
-  ledcSetup(pwmChannel[0], freq[0], resolution);
-  ledcSetup(pwmChannel[1], freq[1], resolution);
-
-  ledcAttachPin(motorPin_speed[0], pwmChannel[0]);
-  ledcAttachPin(motorPin_speed[1], pwmChannel[1]);
+  processMovement(-1);
 
   Serial.begin(115200);           // make sure your Serial Monitor is also set at this baud rate.
   Dabble.begin("ESPController");  //set bluetooth name of your device
 }
 
 void loop() {
+  int movement = -1;
   Dabble.processInput();
 
   if (GamePad.isUpPressed()) {
     
     if (mode) {
-      blink(10 , 25);
-      processMovement(0);
-      delay(movementDelay);
-      processMovement(-1);
+      movement = 0;
     } else {
       Serial.println("Selected servo 1");
-      servoSelected = 0;
+      servo = 0;
       blink(1 , 125);
+    }
+  }
+  
+  if (GamePad.isRightPressed()) {
+    if (mode) {
+      movement = 3;
+    } else {
+    Serial.println("Selected servo 2");
+    servo = 1;
+    blink(2 , 125);
     }
   }
 
   if (GamePad.isDownPressed()) {
     if (mode) {
-      blink(10 , 25);
-      processMovement(1);
-      delay(movementDelay);
-      processMovement(-1);
+      movement = 1;
     } else {
     Serial.println("Selected servo 3");
-    servoSelected = 2;
+    servo = 2;
     blink(3 , 125);
     }
   }
 
   if (GamePad.isLeftPressed()) {
     if (mode) {
-      blink(10 , 25);
-      processMovement(2);
-      delay(movementDelay);
-      processMovement(-1);
+      movement = 2;
     } else {
     Serial.println("Selected servo 4");
-    servoSelected = 3;
+    servo = 3;
     blink(4 , 125);
     }
   }
 
-  if (GamePad.isRightPressed()) {
-    if (mode) {
-      blink(10 , 25);
-      processMovement(3);
-      delay(movementDelay);
-      processMovement(-1);
-    } else {
-    Serial.println("Selected servo 2");
-    servoSelected = 1;
-    blink(2 , 125);
-    }
+  
+  if (GamePad.isSquarePressed())
+  {
+    Serial.println("Selected servo 5");
+    servo = 4;
+    blink(5 , 125);
+  }
+
+  if (GamePad.isCirclePressed())
+  {
+    Serial.println("Selected servo 6");
+    servo = 5;
+    blink(6 , 125);
   }
 
   if (GamePad.isCrossPressed()) {
+    blink(1 , 125);
     moveServo(false);
   }
 
   if (GamePad.isTrianglePressed()) {
+    blink(1 , 125);
     moveServo(true);
   }
-
-  // if (GamePad.isSquarePressed()) {
-  //   Serial.print("Square");
-  // }
-
-  // if (GamePad.isCirclePressed()) {
-  //   Serial.print("Circle");
-  // }
 
   if (GamePad.isSelectPressed()) {
     mode = !mode;
@@ -137,23 +135,36 @@ void loop() {
     processMovement(-1);
     }
   }
+
+  processMovement(movement);
+
+  if(debug && mode) {
+      Serial.println(String(digitalRead(motorPin_fwd[0])) + " , " + String(motorPin_bkwrd[0]) + " , " + String(motorPin_fwd[1]) + " , " + String(motorPin_bkwrd[1]));
+      delay(700);
+  }
 }
 
 void moveServo(bool up) {
 
-  if (angle[servoSelected] == 180 && up) {
-    angle[servoSelected] = 0;
-  } else if (angle[servoSelected] == 0 && (!up)) {
-    angle[servoSelected] = 180;
+  if((value[servo] == max_val[servo]) && up) {
+    if(debug) {
+      Serial.println("Servo " + String(servo + 1) + " reached its max value.");
+    }
+    return; 
   }
-  if (up) {
-    servo[servoSelected].write(++angle[servoSelected]);
-    delay(servoDelay[servoSelected]);
-  } else {
-    servo[servoSelected].write(--angle[servoSelected]);
-    delay(servoDelay[servoSelected]);
+  if((value[servo] == min_val[servo]) && (!up)) {
+    if(debug) {
+      Serial.println("Servo " + String(servo + 1) + " reached its min value.");
+    }
+    return;
   }
-  Serial.println("Angle of Servo " + String(servoSelected + 1) + " :" + String(angle[servoSelected]));
+  if(up) {++value[servo];} else {--value[servo];}
+
+  servoDriver.setPWM(servo, 0, map(value[servo], 0, 180, servoMIN, servoMAX));
+  delay(servoDelay[servo]);
+  if(debug) {
+  Serial.println("Angle of Servo " + String(servo + 1) + " :" + String(value[servo]));
+  }
 }
 
 void blink(uint8_t n , uint16_t d) {
@@ -171,31 +182,27 @@ void blink(uint8_t n , uint16_t d) {
 void processMovement(byte dirn) {
   switch(dirn) {
 
-    case 0 :  Serial.println("Move forward");
+    case 0 :  if(debug) {Serial.println("Move forward");}
               moveMotor(0 , 1);
               moveMotor(1 , 1);
-              changeSpeed(255, 255);
               break;
 
-    case 1 :  Serial.println("Move backward");
-              moveMotor(0 , -1);
-              moveMotor(1 , -1);
-              changeSpeed(255, 255);
+    case 1 :  if(debug) {Serial.println("Move backward");}
+              moveMotor(0 , 2);
+              moveMotor(1 , 2);
               break;
 
-    case 2 :  Serial.println("Move left");
+    case 2 :  if(debug) {Serial.println("Move left");}
               moveMotor(0 , 1);
-              moveMotor(1 , 0);
-              changeSpeed(255, 255);
+              moveMotor(1 , 2);
               break;
               
-    case 3 :  Serial.println("Move right");
-              moveMotor(0 , 0);
+    case 3 :  if(debug) {Serial.println("Move right");}
+              moveMotor(0 , 2);
               moveMotor(1 , 1);
-              changeSpeed(255, 255);
               break;
               
-    default : Serial.println("STOP");
+    default : if(debug){Serial.println("STOP");}
               moveMotor(0, 0);
               moveMotor(1, 0);
               break;
@@ -213,16 +220,12 @@ void moveMotor(byte motor , byte direction) {
               digitalWrite(motorPin_bkwrd[motor], LOW);
               break;
 
-    case -1 : digitalWrite(motorPin_fwd[motor], LOW);
+    case 2 :  digitalWrite(motorPin_fwd[motor], LOW);
               digitalWrite(motorPin_bkwrd[motor], HIGH);
               break;
+
     default : digitalWrite(motorPin_fwd[motor],LOW);
               digitalWrite(motorPin_bkwrd[motor], LOW);
               break;
   }
-}
-
-void changeSpeed(uint8_t speed1 , uint8_t speed2) {
-  ledcWrite(motorPin_speed[0], speed1);
-  ledcWrite(motorPin_speed[1], speed2);
 }
